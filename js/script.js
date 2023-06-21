1af1e4bff7d9534ce80f8893e5a0b8f879afc53f
Elite6277 <hacklinux.93@gmail.com>
2023-06-21 14:25:48 +0500

    JavaScript-loops2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 "use strict";
 
 
-let num = 50;
-
-//while (num <= 55) {
-//   console.log(num)
-//   num++;
-//}
-
-//сначала что то делает потом проаеряет условия
-
-//do {
-//   console.log(num);
-//   num++;
-//}
-//while (num < 55);
-
-
-for (let i = 1; i < 8; i++) {
-   if (i == 6) {
-      //break;
-      continue;
-   }
+for (let i = 0; i < 3; i++) {
    console.log(i);
+   for (let j = 0; j < 3; j++) {
+      console.log(j);
+   }
+}
+
+//*
+//**
+//***
+//****
+//*****
+//******
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+   for (let j = 0; j < i; j++) {
+      result += '*';
+   }
+
+   result += '\n'
+}
+
+console.log(result);
+
+
+first: for (let i = 0; i < 3; i++) {
+   console.log(`First level: ${i}`);
+   for (let j = 0; j < 3; j++) {
+      console.log(`Second level: ${j}`);
+      for (let k = 0; k < 3; k++) {
+         if (k === 2) break first;
+         console.log(`Third level: ${k}`);
+      }
+   }
 }
