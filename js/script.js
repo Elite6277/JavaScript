8b69b45e626a3aa4cedcc94ca9a1d01da7264835
Elite6277 <hacklinux.93@gmail.com>
2023-07-07 22:52:49 +0500

    JavaScript-conditions2_V2.0

--- js/script.js
+++ js/script.js
@@ -76,3 +76,65 @@ console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets)
 
 // Сначала он превращает аргумент к которому применен превращает его в логический тип данных
 console.log(!0);
+
+
+console.log(NaN || 2 || undefined); // 2
+
+
+console.log(NaN && 2 && undefined); // Nan
+
+
+console.log(1 && 2 && 3); //  3
+
+
+console.log(!1 && 2 || !3); //  false
+
+
+console.log(25 || null && !3); // 25
+
+
+console.log(NaN || null || !3 || undefined || 5); // 5
+
+
+console.log(NaN || null && !3 && undefined || 5); // 5
+
+
+console.log(5 === 5 && 3 > 1 || 5); // true
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+//+yes
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+//+yes
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+} else {
+   console.log('adsadas');
+}
+
+//-N0
