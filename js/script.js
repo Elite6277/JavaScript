0d56a574db3222e00e47992e414b1f50b9575716
Elite6277 <hacklinux.93@gmail.com>
2023-07-07 17:26:31 +0500

    interpolation-js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,9 @@
 "use strict";
+//интерполяция в java script
 
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
 
-for (let i = 0; i < 3; i++) {
-   console.log(i);
-   for (let j = 0; j < 3; j++) {
-      console.log(j);
-   }
-}
+const user = "Medet";
 
-//*
-//**
-//***
-//****
-//*****
-//******
-
-let result = '';
-const length = 7;
-
-for (let i = 1; i < length; i++) {
-   for (let j = 0; j < i; j++) {
-      result += '*';
-   }
-
-   result += '\n'
-}
-
-console.log(result);
-
-
-first: for (let i = 0; i < 3; i++) {
-   console.log(`First level: ${i}`);
-   for (let j = 0; j < 3; j++) {
-      console.log(`Second level: ${j}`);
-      for (let k = 0; k < 3; k++) {
-         if (k === 2) break first;
-         console.log(`Third level: ${k}`);
-      }
-   }
-}
+alert(`Привет, ${user}`)
