281afa8d81568b48290192f12464eb742e574b37
Elite6277 <hacklinux.93@gmail.com>
2023-07-07 23:28:33 +0500

    JavaScript-loops-V2.0

--- js/script.js
+++ js/script.js
@@ -1,140 +1,57 @@
 "use strict";
 
-//Логические операторы conditions
-//В JavaScript динамическая типизация
-//В JavaScript типы данных могут переходить один в другой
-const hamburger = true;
-const fries = false;
+//Циклы Loops
+//Циклы используются для повторения однотипных дейсвий
 
-if (hamburger && fries) {
-   console.log('Я сыт!');
+let num = 50;
+
+//Цикл while самый простой цикл
+while (num < 55) {
+   console.log(num);
+   num++;
 }
 
-//Результат логических операций это какое то логическое bool значение
-console.log((hamburger && fries))
+// C помошью do мы можем снала что то сделать потом проверить условие условие;
+//Повторение в программировани называется итерация
 
-// В JavaScript есть 5 сущностей которые будут всегда false
-// Это 0, пустая строка-->'', null, undefined, Nan
+do {
+   console.log(num);
+   num++
+}
+while (num < 55);
 
-// Всё остальное всегда будет true
-// 1 в логичкском контексте всегда true
+//for самый часто использующийся цикл потому что его можно подробно настроить
+// i сокращенно итератор
+// 1 с чего начинать; 2 условие при котором на цикл остановит свою работу;3 шаг цикла;
 
-const hamburger = 3;
-const fries = 1;
-const cola = 1;
-
-console.log(hamburger === 3 && cola && fries);
-// Оператор и возвращает первое ложное значение и запинается,  а если true возвращается последнее правдивое значение
-console.log(1 && 0);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(0 && 'ldas;ldkal;sdk');
-
-const hamburger = true;
-const fries = false;
-
-if (hamburger === 3 && cola === 1 && fries) {
-   console.log('Все сыты');
-} else {
-   console.log('Мы уходим');
+let number = 55;
+for (let i = 1; i < 8; i++) {
+   console.log(number)
+   number++
 }
 
-// Если есть сомнения  в приорите выполнения  то взгляни в таблицу сравнения операторов
-
-
-//Как только оператор или || видит правду он перестаёт работать
-// Код нижe выолнился до hamburger
-const hamburger = 3;
-const fries = 0;
-const cola = 0;
-if (hamburger || cola || fries) {
-   console.log('Все довольны');
-} else {
-   console.log('Мы уходим');
+//Пример бесконечный цикл
+let number = 55;
+for (let i = 1; i < 0; i++) {
+   console.log(number)
+   number++
 }
-// Если в или все неправда то возвращается последнее ложное значение
-//Код нижу верне первую правду
-let johnReport, alexReport, samReport, mariaReport = 'end';
+let number = 55;
 
-console.log(johnReport || alexReport || samReport || mariaReport)
-
-// Комбинация условий
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-   console.log('Все довольны');
-} else {
-   console.log('Мы уходим');
+//Чтобы закончить цикл на определенном этапе  используется break и дальше он не продолжается
+for (let i = 1; i < 10; i++) {
+   if (i == 7) {
+      break;
+   }
+   console.log(i)
+}
+//Оператор continue  позволяет нам пропустить шаг который нам не нужен но при это не прерывает наш цикл
+for (let i = 1; i < 10; i++) {
+   if (i == 7) {
+      continue;
+   }
+   console.log(i)
 }
 
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets)
 
-//Оператор не !
-
-// Сначала он превращает аргумент к которому применен превращает его в логический тип данных
-console.log(!0);
-
-
-console.log(NaN || 2 || undefined); // 2
-
-
-console.log(NaN && 2 && undefined); // Nan
-
-
-console.log(1 && 2 && 3); //  3
-
-
-console.log(!1 && 2 || !3); //  false
-
-
-console.log(25 || null && !3); // 25
-
-
-console.log(NaN || null || !3 || undefined || 5); // 5
-
-
-console.log(NaN || null && !3 && undefined || 5); // 5
-
-
-console.log(5 === 5 && 3 > 1 || 5); // true
-
-
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger === 3 && cola || fries === 3 && nuggets) {
-   console.log('Done!')
-}
-
-//+yes
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger || cola || fries === 3 || nuggets) {
-   console.log('Done!')
-}
-//+yes
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!')
-} else {
-   console.log('adsadas');
-}
-
-//-N0
+//не путать с методами перебора for each for map и тд
