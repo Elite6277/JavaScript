cf0f1357b7c5bd8aba872f8f4b70c9334af4d2aa
Elite6277 <hacklinux.93@gmail.com>
2023-07-14 22:03:59 +0500

    JavaScript-callback

--- js/script.js
+++ js/script.js
@@ -1,96 +1,51 @@
 "use strict";
-/* Задание на урок:
 
-1) Первую часть задания повторить по уроку
+// Call back функции которые используются в неочевидных местах и конструкция
+// Используется очень активно
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
+function first() {
+   // Do something
+   setTimeout(function () {
+      console.log(1);
 
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
-// Код возьмите из предыдущего домашнего задания
-
-
-let numberOfFilms;
-
-function start() {
-   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели')
-
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-      numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели');
-   }
+   }, 500);
 }
 
-start();
-
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false,
+function second() {
+   console.log(2);
 }
 
+first();
+second();
 
+//Главное правило если функции идут одна за другой это не значит что они выполняются так же
+//они запускаются одна за другой но могут выполнится в разное время
 
-function rememberMyFilms() {
-   for (let i = 0; i < 2; i++) {
-      //Один из полезных методов у строк  метод trim() который удаяляет пробелы с начала и конца строки
-      const lastSeenFilm = prompt('Один из последних просмотренных фильмов?', '').trim();
-      const filmsRaiting = prompt('На сколько оцените его?');
-      if (lastSeenFilm != null && filmsRaiting != null && lastSeenFilm != '' && filmsRaiting != '' && lastSeenFilm.length < 50 && filmsRaiting.length <= 3) {
-         personalMovieDB.movies[lastSeenFilm] = filmsRaiting;
-         console.log('done');
-      } else {
-         console.log('error');
-         i--;
-      }
-   }
-}
-rememberMyFilms();
+//Сall back это функция которая должна быть выполнена после того как другая фнкция завершит своё выполнение
 
-
-function detectPersonalLevel() {
-   if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
-      alert('Просмотрено довольно мало фильмов');
-   } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-      alert('Вы классический зритель');
-   } else if (personalMovieDB.count > 30) {
-      alert('Вы киноман');
-   } else {
-      alert('Произошла ошибка');
-   }
+function learnJS(lang, callback) {
+   // Это главный шаблон callback функций
+   // То что в другую функцию в качестве аргумента , в будущем мы можем  передать другую функцию
+   console.log(`Я учу: ${lang}`);
+   callback();
 }
 
-detectPersonalLevel();
+learnJS('JavaScript', function () {//Первый раз когда встречаемся с анонимной функцией
+   console.log('Я прошел этот урок!')
+});
 
-function showMyDB(hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB);
-   }
+// Второй вариант
+function learnJS(lang, callback) {
+   // Это главный шаблон callback функций
+   // То что в другую функцию в качестве аргумента , в будущем мы можем  передать другую функцию
+   console.log(`Я учу: ${lang}`);
+   callback();
 }
 
-showMyDB(personalMovieDB.privat);
-
-
-//function writeYourGenres() {
-//   for (let i = 1; i <= 3; i++) {
-//      const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-//      personalMovieDB.genres[i - 1] = genre;//делаем так чтобы i  начинался с нуля потому что в масииве все начинается с нуля 
-//   }
-//}
-
-//Более оптимизированный вариант
-function writeYourGenres() {
-   for (let i = 1; i <= 3; i++) {
-      personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);//делаем так чтобы i  начинался с нуля потому что в масииве все начинается с нуля 
-   }
+function done() {
+   console.log('Я прошел этот урок!');
 }
 
-writeYourGenres();
+learnJS('JavaScript', done);
 
-// В каждую функцию можно передавать то с чем она будет работать
+// Итог callbackи позволяют нам быть уверенным в том что определенный код не начнет свое исполнение до того момента пока другой код не завершит свое исполнение
