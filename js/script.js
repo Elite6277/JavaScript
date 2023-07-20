f900c8d2ddab79abf6667cf9c1ad4800c2b7d033
Elite6277 <hacklinux.93@gmail.com>
2023-07-21 01:45:21 +0500

    JavaScript-recursion

--- js/script.js
+++ js/script.js
@@ -1,50 +1,158 @@
 'use strict';
 
-// Как путешествовать по дом дереву при помощи js
+// Рекурсия прием который используется в программировании в целом
+// Внутри функции могут вызываться другие функции
+// Если ту же функцию вызывать внутри себя же это назывется рекурсией
 
-//console.log(document.body); // Обращаемся к тегу body
-//console.log(document.documentElement); // ОБращаемся к тегу html
-console.log(document.body.childNodes);
+// Классический пример это функция возведения в степень
+//function pow(x, n) {
+//   let result = 1;
 
-// Родительский элемент это тот который объединяет другие находящиеся внутри него их мы тоже можем получить при помощи трех команд
+//   for (let i = 0; i < n; i++) {
+//      result *= x;
+//   }
 
-// !Самая полезная в практической деятольности это метод childNodes
-//! Разница между дом элементами и дом узлами каждая сущнос которая здесь находится будет узлом но не каждый узел будет элементом
+//   return result;
+//}
 
-// Методы при помощи которых можно путешествовать по дом дереву 
-// 1 ChildNodes позволяет нам получить все ноды которые находятся внутри родителя  
-// 1 firstChild позволяет нам получить все ноды которые находятся внутри родителя  
-// 1 lastChild позволяет нам получить все ноды которые находятся внутри родителя  
+function pow(x, n) {
+   if (n === 1) {
+      return x;
+   } else {
+      return x * pow(x, n - 1);// рекурсия это когда функция внутри себя сама себя запускает для каких то действий
+   }
+}
 
-console.log(document.body.firstChild); //Получем первого ребенка внутри родителя 
-console.log(document.body.firstElementChild); //Получем первого ребенка внутри родителя 
-console.log(document.body.lastChild); // Получаем последнего ребенка внутри родителя
+// Термины котрые могут пригодится
+// 1термин база рекурсии это случай котрый приводит сразу к завершению функции
+// 2термин шаг рекурсии это запуск вложенной функции но с другим значением
+// 3термин глубина рекурсии общее количество вложенных вызовов вместе с самым первым
 
-//Отталкиваемся абсолютно от любого элемента на странице
+// Итеративный подход тоесть цикл он эфетивнее рекурсии это связано с внутренним устройством языка 
+// Рекурсия делает функции проще  хоть и имеет ограничения по глубине тоесть по количеству внутренних вызовов
 
-// Мы поработаем с командами которые позволяют получить нам родителя соседей и детей
+//! Большинство программистов склоняются в сторону рекурсии
 
-//Если мы элемент прдпологаем использовать только один раз нам его не нужно помешать в какую то переменную
-console.log(document.querySelector('#current').parentNode.parentNode);
-console.log(document.querySelector('#current').parentElement);
 
-// Когда мы прописываем различные скрипты нам иногда не хватает каких то ориентиров для этого были  созданы data атрибуты
+pow(2, 1)//2
+pow(2, 2)//4
+pow(2, 3)//8
+pow(2, 4)//16
 
-// Синтаксис у них таков что мы всегда начинаем его с data- а то что после дефиза абсолютно произвольное
-// И на эти дата атрибуты которые мы сами проставляем уже в верстке очень удобно ориентироваться в наших скриптах и этим мы удем очень часто позоваться
+let students = {
+   js: [{
+      name: 'John',
+      progress: 100
+   }, {
+      name: 'Ivan',
+      progress: 60
+   }],
 
-// Чтобы получить следующий за этим элементом элемент мы испоьзуем свойсво nextSibling
-// Чтобы получить предыдущий элемент мы испоьзуем свойсво previousSibling
-console.log(document.querySelector('[data-current="3"]').nextSibling);// Чтобы получить какой то аттрибут Html мы используем квадратные скобки
-console.log(document.querySelector('[data-current="3"]').nextElementSibling);// Чтобы получить какой то аттрибут Html мы используем квадратные скобки
+   html: {
+      basic: [{
+         name: 'Peter',
+         progress: 20
+      }, {
+         name: 'Ann',
+         progress: 18
+      }],
 
-//Чтобы не натыкаться на ноды у этих коман есть аналоги которые ссылаются уже на конкретный элемент
+      pro: [{
+         name: 'Sam',
+         progress: 10
+      }]
+   }
+};
 
-for (let node of document.body.childNodes) {
-   if (node.nodeName == '#text') {
-      continue;
+// Задача посчитать средний прогресс всех студентов всех курсов
+// 1 Понять сколько студентов в общем 
+// 2 и общее число в процентах
+
+function getTotalProgressByIteration(data) {
+   let total = 0;
+   let students = 0;
+
+
+   // Очень полезный метод для объктов который наз-ся object.values()
+   // Этот метод позволяет получить массив значений перечисляемых свойств объекта в том же порядке  что и если бы работал for in цикл
+
+   for (let course of Object.values(data)) {
+      if (Array.isArray(course)) {
+         students += course.length;
+
+         for (let i = 0; i < course.length; i++) {
+            total += course[i].progress;
+         }
+      } else {
+         for (let subCourse of Object.values(course)) {
+            students += subCourse.length;
+            for (let i = 0; i < subCourse.length; i++) {
+               total += subCourse[i].progress;
+            }
+         }
+
+      }
+      //метод Array.isArray() проверяет является ли что то массивом
    }
 
-
-   console.log(node);
+   return total / students;
 }
+
+console.log(getTotalProgressByIteration(students));
+
+// Рекурсия упрощает функцию потому что мы все однотипные дейсвтвия заключем в функцию и она внутри себя ее каждый раз вызывает
+// Второй вариант при помощи рекурсии
+let students = {
+   js: [{
+      name: 'John',
+      progress: 100
+   }, {
+      name: 'Ivan',
+      progress: 60
+   }],
+
+   html: {
+      basic: [{
+         name: 'Peter',
+         progress: 20
+      }, {
+         name: 'Ann',
+         progress: 18
+      }],
+
+      pro: [{
+         name: 'Sam',
+         progress: 10
+      }]
+   }
+};
+function getTotalProgressByRecursion(data) {
+   if (Array.isArray(data)) {
+      let total = 0;
+
+      for (let i = 0; i < data.length; i++) {
+         total += data[i].progress;
+      }
+      return [total, data.length];//При помощи return можно венрнуть все что угодно в том числе и массив с данными
+      // Базоыве понятия
+      // 1термин база рекурсии это случай котрый приводит сразу к завершению функции
+      //Базу мы написали
+   } else {
+      let total = [0, 0];
+
+      for (let subData of Object.values(data)) {
+         const subDataArr = getTotalProgressByRecursion(subData);
+         total[0] += subDataArr[0];
+         total[1] += subDataArr[1];
+      }
+      return total;
+   }
+}// 2термин шаг рекурсии это запуск вложенной функции но с другим значением
+
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0] / result[1]);
+
+
+
+// 3термин глубина рекурсии общее количество вложенных вызовов вместе с самым первым
