f7e86f2d7be43acfcc29d00d404437e7bbd10e48
Elite6277 <hacklinux.93@gmail.com>
2023-07-24 23:16:45 +0500

    JavaScript-(WeakMap and WeakSet)

--- js/script.js
+++ js/script.js
@@ -1,100 +1,60 @@
 "use strict"
 
-// Скрипты и время их выполнения.setTimeout setInterval
+//(WeakMap и WeakSet)
 
-// Для того чтобы запустить наш код через определенный промежуток времени нам нужна конструкция которая называется setTimeout
+//let user = { name: 'Ivan' };
 
-//setTimeout() у этой функции такой синтаксис что сначала она принимает ту функцию которая должна будет запуститься через определенный промежуток времени
-// Она принимает либо объявление функции либо ее название она не выполняется здесь и сейчас
+//let map = new WeakMap();
+//map.set(user, 'data');
 
-//Синтаксис который позволяет запустить любую операцию с течением времени
-//Помимо этого данная  конструкция может принимать следующие аргументы  которые будет уже аргументами для функции внутри
-//const timerId = setTimeout(function () {
-//   console.log('hello');
-//}, 2000);
+//user = null;
 
-//const timerId = setTimeout(function (text) {
-//   console.log(text);
-//}, 2000, 'Hello');// аргументов может быть бесконечное кол-во
-// время в таких Timeout устанавливается в милисекунде
+//console.log(map.has(user));
 
+let cache = new WeakMap();
 
-//Первым аргументом вместо того чтобы здесь описывать какую то функцию просто передаем уже готовую функцию
-//! Заметье что мы его здесь не вызываем() мы просто ставим название функции
-//const timerId = setTimeout(logger, 2000); //пройдет две секунды и потом у нас сработает функция logger
-//Мы можем передавать уже готовую функцию
-//function logger() {
-//   console.log('text');
-//}
-
-//SetTimout работает и самостоятельно ее не обязательно присваивать переменной
-//setTimeout(logger, 2000); //пройдет две секунды и потом у нас сработает функция logger
-//Мы можем передавать уже готовую функцию
-//function logger() {
-//   console.log('text');
-//}
-
-// setTimeout присваивается в переменную для того чтобы мы могли определять различные setTimeoutы потому что таких ассинхронных функций у нас может быть очень много, А определять нам их нужно чтобы в будущем нам их как-то останавливать 
-//const timerId = setTimeout(logger, 2000); //пройдет две секунды и потом у нас сработает функция logger
-//Мы можем передавать уже готовую функцию
-
-//function logger() {
-//   console.log('text');
-//}
-//const timerId = setTimeout(logger, 2000);
-
-////Для того чтобы сбросить timeout используется clearInterval()
-//clearInterval(timerId);
-
-
-
-//function logger() {
-//   console.log('text');
-//}
-
-const btn = document.querySelector('.btn');
-let timerId,
-   i = 0;
-
-// делаем анимацию для кубика
-function myAnimation() {
-   const elem = document.querySelector('.box');
-   let pos = 0;
-
-   const id = setInterval(frame, 10);
-   function frame() {
-      if (pos == 300) {
-         clearInterval(id);
-      } else {
-         pos++;
-         elem.style.top = pos + 'px';
-         elem.style.left = pos + 'px';
-      }
+function cacheUser(user) {
+   if (!cache.has(user)) {
+      cache.set(user, Date.now());
    }
+   return cache.get(user);
 }
 
-btn.addEventListener('click', myAnimation);
+let lena = { name: 'Elena' };
+let alex = { name: 'Alex' };
 
-//btn.addEventListener('click', () => {
-   //const timerId = setTimeout(logger, 2000);
-   //Чтобы код работал постоянно каждые две секунды используем setInterval
-//   timerId = setInterval(logger, 2000);
+cacheUser(lena);
+cacheUser(alex);
 
-//});
+lena = null;
 
-//clearInterval(timerId);
+console.log(cache.has(lena));
+console.log(cache.has(alex));
 
-//function logger() {
-//   if (i == 3) {
-//      clearInterval(timerId);
-//   }
-//   console.log('text');
-//   i++;
-//}
+// WeakSet
+// add, has, delete
 
-//Для избежания ошибок пишут рекурсивный метод setTimeOut();
 
-//let id = setTimeout(function log() {
-//   console.log('hello');
-//   id = setTimeout(log, 500)
-//}, 500); // Эта обертка будет ждат пока внутренний код полностью выполнится потом только начнет действовать
+let messages = [
+   { text: 'Hello', from: 'John' },
+   { text: 'World', from: 'Alex' },
+   { text: '...', from: 'M' },
+];
+
+// Weak Set помогает нам хранить набор каких то данных и легко проверять
+//Weak Set Очищается автомотически
+let readMessages = new WeakSet();
+
+readMessages.add(messages[0]);
+//readMessages.add(messages[1]);
+
+
+readMessages.add(messages[0]);
+messages.shift();
+
+console.log(readMessages.has(messages[0]));
+
+//WeakMap и WeakSet выполняют задачу того что они являются дополнительным хранилищем  управляемых из каких то других мест в коде
+// ЗДесь мы работаем с объектами если на них нет ссылок и они содержаться только в этих структурах то они будут удалены из памяти
+
+// Эти структуры данных они контролируют  использование памяти и всегда нужно помнить про ограничения с ними
