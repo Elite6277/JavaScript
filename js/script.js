bc9c5a44bebbdadc98cada4a5bcb601d5f646e48
Elite6277 <hacklinux.93@gmail.com>
2023-07-24 20:26:41 +0500

    JavaScript-Scripts_and_their_execution_time_setTimeout_setInterval

--- js/script.js
+++ js/script.js
@@ -1,57 +1,100 @@
 "use strict"
 
-// В программировании много путей решения задач и чаще всего выбирают между более понятным и более простым в работе или по параметру скорости 
+// Скрипты и время их выполнения.setTimeout setInterval
+
+// Для того чтобы запустить наш код через определенный промежуток времени нам нужна конструкция которая называется setTimeout
+
+//setTimeout() у этой функции такой синтаксис что сначала она принимает ту функцию которая должна будет запуститься через определенный промежуток времени
+// Она принимает либо объявление функции либо ее название она не выполняется здесь и сейчас
+
+//Синтаксис который позволяет запустить любую операцию с течением времени
+//Помимо этого данная  конструкция может принимать следующие аргументы  которые будет уже аргументами для функции внутри
+//const timerId = setTimeout(function () {
+//   console.log('hello');
+//}, 2000);
+
+//const timerId = setTimeout(function (text) {
+//   console.log(text);
+//}, 2000, 'Hello');// аргументов может быть бесконечное кол-во
+// время в таких Timeout устанавливается в милисекунде
 
 
-//Добавляем табы 
+//Первым аргументом вместо того чтобы здесь описывать какую то функцию просто передаем уже готовую функцию
+//! Заметье что мы его здесь не вызываем() мы просто ставим название функции
+//const timerId = setTimeout(logger, 2000); //пройдет две секунды и потом у нас сработает функция logger
+//Мы можем передавать уже готовую функцию
+//function logger() {
+//   console.log('text');
+//}
 
-// Назначение глобального обработчика событий DOMContentloaded
-window.addEventListener('DOMContentLoaded', () => {
+//SetTimout работает и самостоятельно ее не обязательно присваивать переменной
+//setTimeout(logger, 2000); //пройдет две секунды и потом у нас сработает функция logger
+//Мы можем передавать уже готовую функцию
+//function logger() {
+//   console.log('text');
+//}
 
-   // У нас есть три задачи
-   // первое это функция которая будет скрывать ненужные нам табы
-   // Показать нужный таб
-   // назначить обработчики событий на меню
+// setTimeout присваивается в переменную для того чтобы мы могли определять различные setTimeoutы потому что таких ассинхронных функций у нас может быть очень много, А определять нам их нужно чтобы в будущем нам их как-то останавливать 
+//const timerId = setTimeout(logger, 2000); //пройдет две секунды и потом у нас сработает функция logger
+//Мы можем передавать уже готовую функцию
 
-   const tabs = document.querySelectorAll('.tabheader__item'),
-      tabsContent = document.querySelectorAll('.tabcontent'),
-      tabsParent = document.querySelector('.tabheader__items');
+//function logger() {
+//   console.log('text');
+//}
+//const timerId = setTimeout(logger, 2000);
 
-   function hideTabContent() {
-      tabsContent.forEach(item => {
-         item.classList.add('hide');
-         item.classList.remove('show', 'fade');
-      });
-
-      tabs.forEach(item => {
-         item.classList.remove('tabheader__item_active');
-      });
-   }
-
-   //Если функция вызывается без аргумента  то по уолчанию отработает то что присвоили  i 
-   function showTabContent(i = 0) {
-      tabsContent[i].classList.add('show', 'fade');
-      tabsContent[i].classList.remove('hide');
-      tabs[i].classList.add('tabheader__item_active');
-   }
+////Для того чтобы сбросить timeout используется clearInterval()
+//clearInterval(timerId);
 
 
-   hideTabContent();
-   showTabContent();
 
-   tabsParent.addEventListener('click', (event) => {
-      const target = event.target;
+//function logger() {
+//   console.log('text');
+//}
 
-      if (target && target.classList.contains('tabheader__item')) {
-         tabs.forEach((item, i) => {
-            if (target == item) {
-               hideTabContent();
-               showTabContent(i);
-            }
-         })
+const btn = document.querySelector('.btn');
+let timerId,
+   i = 0;
+
+// делаем анимацию для кубика
+function myAnimation() {
+   const elem = document.querySelector('.box');
+   let pos = 0;
+
+   const id = setInterval(frame, 10);
+   function frame() {
+      if (pos == 300) {
+         clearInterval(id);
+      } else {
+         pos++;
+         elem.style.top = pos + 'px';
+         elem.style.left = pos + 'px';
       }
-   });
+   }
+}
 
+btn.addEventListener('click', myAnimation);
 
+//btn.addEventListener('click', () => {
+   //const timerId = setTimeout(logger, 2000);
+   //Чтобы код работал постоянно каждые две секунды используем setInterval
+//   timerId = setInterval(logger, 2000);
 
-});
+//});
+
+//clearInterval(timerId);
+
+//function logger() {
+//   if (i == 3) {
+//      clearInterval(timerId);
+//   }
+//   console.log('text');
+//   i++;
+//}
+
+//Для избежания ошибок пишут рекурсивный метод setTimeOut();
+
+//let id = setTimeout(function log() {
+//   console.log('hello');
+//   id = setTimeout(log, 500)
+//}, 500); // Эта обертка будет ждат пока внутренний код полностью выполнится потом только начнет действовать
