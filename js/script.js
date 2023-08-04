5ec8b1c1a97fa8c5cb138dc7f9a80b52f1dc1944
Elite6277 <hacklinux.93@gmail.com>
2023-08-04 16:28:00 +0500

    JavaScript_Slider_3variant_with_dots

--- js/script.js
+++ js/script.js
@@ -489,6 +489,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
    //Берем класс который определяет каждый отдельный слайд
    const slides = document.querySelectorAll('.offer__slide'),
+      //Получаем слайдер 
+      slider = document.querySelector('.offer__slider'),
       // Стрелочка prev 
       prev = document.querySelector('.offer__slider-prev'),
       // Стрелочка next 
@@ -524,6 +526,53 @@ window.addEventListener('DOMContentLoaded', () => {
       slide.style.width = width;
    });
 
+   slider.style.position = 'relative'
+
+   //  Создаем большую обертку для наших точек  как то их стилизуем 
+   const indicators = document.createElement('ol'),
+      dots = [];
+
+   indicators.classList.add('carousel-indicators');
+   indicators.style.cssText = `
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 15;
+      display: flex;
+      justify-content: center;
+      margin-right: 15%;
+      margin-left: 15%;
+      list-style: none;
+   `;
+   slider.append(indicators);
+
+   for (let i = 0; i < slides.length; i++) {
+      const dot = document.createElement('li');
+      dot.setAttribute('data-slide-to', i + 1);
+      dot.style.cssText = `
+         box-sizing: content-box;
+         flex: 0 1 auto;
+         width: 30px;
+         height: 6px;
+         margin-right: 3px;
+         margin-left: 3px;
+         cursor: pointer;
+         background-color: #fff;
+         background-clip: padding-box;
+         border-top: 10px solid transparent;
+         border-bottom: 10px solid transparent;
+         opacity: .5;
+         transition: opacity .6s ease;
+      `;
+
+      if (i == 0) {
+         dot.style.opacity = 1;
+      }
+      indicators.append(dot);
+      dots.push(dot);
+   }
+
    // Обработчик события для того чтобы передвигать наш слайдер 
    next.addEventListener('click', () => {
       if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { //'500px'
@@ -544,6 +593,9 @@ window.addEventListener('DOMContentLoaded', () => {
       } else {
          current.textContent = slideIndex;
       }
+
+      dots.forEach(dot => dot.style.opacity = '.5');
+      dots[slideIndex - 1].style.opacity = 1;
    });
 
    //Обработчик события для того чтобы передвигать наш слайдер назад
@@ -566,7 +618,28 @@ window.addEventListener('DOMContentLoaded', () => {
       } else {
          current.textContent = slideIndex;
       }
+      dots.forEach(dot => dot.style.opacity = '.5');
+      dots[slideIndex - 1].style.opacity = 1;
    });
+
+   dots.forEach(dot => {
+      dot.addEventListener('click', (e) => {
+         const slideTo = e.target.getAttribute('data-slide-to');
+
+         slideIndex = slideTo;
+         offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+         slidesField.style.transform = `translateX(-${offset}px)`;
+
+         if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+         } else {
+            current.textContent = slideIndex;
+         }
+         dots.forEach(dot => dot.style.opacity = '.5');
+         dots[slideIndex - 1].style.opacity = 1;
+      });
+   })
 
    // Инициализируем слайдер чтобы ое изначально превратился в ту структуру которую мы должны увидеть 
    // Для этого мы берем наш функциоанал show Slides  и во внутрь мы помещяем наше начальное значение  слайд индекса 
